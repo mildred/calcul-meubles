ccb531f00ffa3fba3a3e14f10fdd70bce8c591f0
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2019-11-19 12:56:06 +0100

    Fix liste debit

--- src/ListeDebit.svelte
+++ src/ListeDebit.svelte
@@ -2,9 +2,28 @@
 
   export let pieces = []
 
-  $: total_cube = pieces.map(p => p.piece.cubage()).reduce((a, b) => (a+b))
+  let cubeprice = 0
+
+  $: total_cube = pieces.map(p => p.que * p.piece.cubage()).reduce((a, b) => (a+b))
+  $: total_prix = pieces.map(p => p.que * p.piece.prix(cubeprice)).reduce((a, b) => (a+b))
 
 </script>
+
+<style>
+  table {
+    border-collapse: collapse;
+  }
+  th {
+    text-align: left;
+  }
+  th, td {
+    border: 1px solid #ddd;
+    padding: 0.2em;
+  }
+  tr:nth-child(even) {
+    background-color: #f2f2f2;
+  }
+</style>
 
 <table>
   <caption>Liste de débit</caption>
@@ -13,7 +32,10 @@
     <th>Qué</th>
     <th>L x l x e</th>
     <th>Arrasement</th>
+    <th>Surface (m²)</th>
+    <th>epaisseur plateau (mm)</th>
     <th>Cubage</th>
+    <th>Prix au m³<br/><input type=number bind:value={cubeprice} size=5 step=10/></th>
   </tr>
   {#each pieces as piece}
   <tr>
@@ -21,7 +43,10 @@
     <td>{piece.que || 1}</td>
     <td>{piece.piece.string_dimentions()}</td>
     <td>{piece.piece.arrasement || ''}</td>
-    <td>plateau {piece.piece.epaisseur_plateau}, {piece.piece.cubage()}</td>
+    <td>{piece.piece.largeur * piece.piece.longueur / 1e6}</td>
+    <td>{piece.piece.epaisseur_plateau}</td>
+    <td>{piece.que * piece.piece.cubage().toPrecision(9)}</td>
+    <td>{piece.que * piece.piece.prix(cubeprice).toPrecision(2)}</td>
   </tr>
   {/each}
   <tr>
@@ -29,6 +54,9 @@
     <td></td>
     <td></td>
     <td></td>
-    <td>{total_cube}</td>
+    <td></td>
+    <td></td>
+    <td>{total_cube.toPrecision(9)}</td>
+    <td>{total_prix.toPrecision(2)}</td>
   </tr>
 </table>
