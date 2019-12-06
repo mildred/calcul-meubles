549692000f30005c365847794185bf0eab27e91a
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2019-12-06 12:10:20 +0100

    Allow saving drawing + fix bottom view

--- src/pieces/SVGDrawing.svelte
+++ src/pieces/SVGDrawing.svelte
@@ -4,14 +4,44 @@
   export let pieces;
   export let zoom = 0.5;
 
+  let svgElement;
+
   $: [xmax, ymax, zmax] = pieces
     .map(p => p.bounding_box())
     .map(b => [b.xmax, b.ymax, b.zmax])
     .reduce((b0, b1) => [Math.max(b0[0], b1[0]), Math.max(b0[1], b1[1]), Math.max(b0[2], b1[2])], [0, 0, 0])
+
+  function save(){
+    let markup = svgElement.outerHTML
+    // TODO: detect filename
+    let filename = (prompt("Nom du fichier :") || "dessin") + ".svg"
+
+    let file = new window.File([markup], filename, {
+      type: 'image/svg+xml'
+    })
+    let url = URL.createObjectURL(file);
+
+    try {
+      let a = document.createElement('a');
+      a.href = url;
+      a.style.display = 'none';
+      a.setAttribute('download', filename);
+
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    } finally {
+      URL.revokeObjectURL(url)
+    }
+  }
 </script>
 
-<p>Zoom : <input type=range bind:value={zoom} min=0 max=1 step=.05> {zoom*100} %</p>
+<p>
+  Zoom : <input type=range bind:value={zoom} min=0 max=1 step=.05> {zoom*100} %
+  <a href="javascript:void(0)" on:click={save}>Enregistrer</a>
+</p>
 <svg
+    bind:this={svgElement}
     data-count={pieces.length}
     width="{5 + zoom*xmax + 5 + zoom*zmax + 5}"
     height="{5 + zoom*ymax + 5 + zoom*zmax + 5}">
@@ -25,9 +55,9 @@
       <SVGItem item={piece} pos="zy" />
     {/each}
   </g>
-  <g transform="translate(5, {5 + zoom*(ymax+zmax) + 5}) scale({zoom} {zoom})">
+  <g transform="translate(5, {5 + zoom*(ymax) + 5}) scale({zoom} {zoom})">
     {#each pieces as piece}
-      <SVGItem item={piece} pos="xz" />
+      <SVGItem item={piece} pos="xZ" />
     {/each}
   </g>
 </svg>
