bb2895e3af0df45749b3ce6313bc9abcf42c4750
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2020-06-11 11:45:00 +0200

    ...

---
+++ src/controls/InputDurationMin.svelte
@@ -0,0 +1,18 @@
+<script>
+
+  export let value = null
+  export let def = null
+  export let size = 5
+  export let step = "any"
+  export let title = "";
+
+</script>
+
+<input
+  style="width: {size+1}em"
+  type=number
+  bind:value={value}
+  placeholder={def}
+  step={step}
+  title={title}
+  /> min
